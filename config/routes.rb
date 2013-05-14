7d6f19e443ed098b237c3685af39fe369ba99f90
Yngve Haugslett <haugslett@gmail.com>
2013-05-14 14:30:06 +0200

    Pretty RSpec

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 SampleApp::Application.routes.draw do
+ root :to => 'static_pages#home'
   get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
-  get "static_pages/contacts"
+  
+  #Static Pages
+  match '/about', to: 'static_pages#about'
+  match '/contact', to: 'static_pages#contact'
+  match '/help', to: 'static_pages#help'
 
 
   # The priority is based upon order of creation:
