0170e6442e6fc85702eb79bdf7db4d7a551fc4f6
Yngve Haugslett <haugslett@gmail.com>
2013-05-14 18:23:45 +0200

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 SampleApp::Application.routes.draw do
+  get "users/new"
+
  root :to => 'static_pages#home'
   get "static_pages/home"
   
@@ -6,6 +8,9 @@ SampleApp::Application.routes.draw do
   match '/about', to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
   match '/help', to: 'static_pages#help'
+
+  #User registration
+  match '/signup', to: 'users#new'
 
 
   # The priority is based upon order of creation:
