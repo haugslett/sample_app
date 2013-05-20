5121fdd9d07831ee6cf2617ce6693e345ecef980
Yngve Haugslett <haugslett@gmail.com>
2013-05-20 18:00:53 +0200

    Finish user edit, update, index, and destroy actions

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -25,9 +25,9 @@ RSpec::Matchers.define :have_success_message do |message|
   end
 end
 
-RSpec::Matchers.define :have_headline do |title|
+RSpec::Matchers.define :have_headline do |h1|
 	match do |page|
-		page.should have_selector('h1', text: title)
+		page.should have_selector('h1', text: h1)
 	end
 end
 
@@ -35,4 +35,13 @@ RSpec::Matchers.define :have_title do |title|
 	match do |page|
 		page.should have_selector('title', text: title)
 	end
+end
+
+def sign_in(user)
+	visit signin_path
+	fill_in "Email",	with: user.email
+	fill_in "Password", with: user.password
+	click_button "Sign in"
+	# Sign in when not using Capybara as well
+	cookies[:remember_token] = user.remember_token
 end
